c01075ff3004cf35d9e11b4009b655ed340c81be
Liu Chang <lllccc2009@gmail.com>
2015-07-30 13:46:17 -0400

    add the sorting for output result

--- listsize.sh
+++ listsize.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-# List the sub-folder size under the parent folder
 
 for i in `ls`
 do
-	echo `du -sh "$i"`
+	echo `du -sh "$i"` >> log.txt
 done
+
+## in order to keep the original format, I need to add " " outside the result of sort
+echo  "`sort -h log.txt`"
+rm log.txt
 	
